b583bcb5947d60d798f8cc2a47c4f163420457e3
Piotr Usewicz <piotr@layer22.com>
2025-09-25 01:35:48 +0200

    Remove clang-tidy
    It's better suited for C++

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,16 +1,4 @@
 if(NOT CMAKE_BUILD_TYPE STREQUAL "Release")
-  find_program(CLANGTIDY clang-tidy)
-  if(CLANGTIDY)
-    message(STATUS "Using clang-tidy, found ${CLANGTIDY}")
-    set(CMAKE_C_CLANG_TIDY "${CLANGTIDY};-extra-arg=-Wno-unknown-warning-option")
-    # Explicitly enable exceptions on Windows
-    if(WIN32)
-      set(CMAKE_C_CLANG_TIDY "${CMAKE_C_CLANG_TIDY};--extra-arg=/EHsc")
-    endif()
-  else()
-    message(WARNING "clang-tidy requested but executable not found")
-  endif()
-
   # This will gradually increase memory usage of the program,
   # discovered on Apple M1, 13.0.
   if(NOT WIN32)
