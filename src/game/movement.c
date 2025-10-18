34661588e63e8e8f34817b17ac8e62630008d0b2
Piotr Usewicz <piotr@layer22.com>
2025-10-18 23:40:52 +0200

    Add missing const

--- src/game/movement.c
+++ src/game/movement.c
@@ -2,7 +2,7 @@
 
 #include <cute_math.h>
 
-void update_movement(CF_V2* position, CF_V2* velocity) {
+void update_movement(CF_V2* position, const CF_V2* velocity) {
     position->x += velocity->x;
     position->y += velocity->y;
 }
--- src/game/movement.h
+++ src/game/movement.h
@@ -2,4 +2,4 @@
 
 typedef struct CF_V2 CF_V2;
 
-void update_movement(CF_V2* position, CF_V2* velocity);
+void update_movement(CF_V2* position, const CF_V2* velocity);
