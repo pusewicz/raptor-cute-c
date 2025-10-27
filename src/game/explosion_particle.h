fd2b3a58abe95f52ccc593c1beba892fc98a9cef
Piotr Usewicz <piotr@layer22.com>
2025-10-27 22:35:40 +0100

    Update explosion_particle.h

--- src/game/explosion_particle.h
+++ src/game/explosion_particle.h
@@ -5,6 +5,8 @@
 #include <cute_sprite.h>
 #include <stddef.h>
 
+typedef enum EnemyType EnemyType;
+
 typedef struct ExplosionParticle {
     CF_V2     position;
     CF_V2     velocity;
@@ -19,6 +21,7 @@ typedef struct ExplosionParticle {
 ExplosionParticle make_explosion_particle(float x, float y, CF_Color color, float angle);
 void              spawn_explosion_particle(ExplosionParticle particle);
 void              spawn_explosion_particles(size_t count, const ExplosionParticle particles[static restrict count]);
-void              spawn_explosion_particle_burst(CF_V2 pos, const CF_Sprite* sprite);
+void              spawn_explosion_particle_burst(CF_V2 pos, const EnemyType enemy_type);
 void              cleanup_explosion_particles(void);
 void              update_explosion_particles(void);
+void              render_explosion_particles(void);
